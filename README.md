f13b1362127e15c467cc353557eaf363efea2172
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2024-12-22 20:28:58 +0600

    i have add post api

---
+++ README.md
@@ -0,0 +1,13 @@
+## My Project Name is MovFix
+
+here is the Link
+
+# [https://movie-fix-server-a-10.vercel.app](https://movie-fix-server-a-10.vercel.app)
+
+### Key Features of my project
+
+- you can get all moves by (/movies).
+- you can get a move by (/movies/id).
+- you can add to card moves by (/status/id)
+- you can delete moves by (/movies/id)
+- you can update moves by (/movies/id)
---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle where
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.SECRET_USER_NAME}:${process.env.SECRET_PASSWORD}@cluster0.ze0za.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const serviceCollection = client.db("ServicesDB").collection("Services");
+
+    // post a service to database
+    app.post("/addService", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Learn skills project's server is running.....");
+});
+
+app.listen(port, () => {
+  console.log(`my app is running on port: ${port}`);
+});
